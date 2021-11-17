c2b7504e207062752bf74803fa51124a5ee6e75b
Pham Tu <phamtu613@gmail.com>
2021-11-17 07:29:16 +0700

    contact res

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $('.project-relative-slider').owlCarousel({
   navText: ['<img srcset="../img/arrow-left-gray.png 2x" alt="" />', '<img srcset="../img/arrow-right.png 2x" alt="" />'],
   responsive: {
     0: {
-      items: 1
+      items: 2
     },
     600: {
       items: 3
@@ -21,18 +21,37 @@ $('.customer-slider').owlCarousel({
   loop: true,
   margin: 30,
   autoplay: true,
-  autoplayTimeout: 993000,
-  nav: true,
+  autoplayTimeout: 3000,
+  nav: false,
   dots: true,
   responsive: {
     0: {
-      items: 1
+      items: 2
     },
     600: {
       items: 3
     },
     1000: {
       items: 5
+    }
+  }
+})
+$('.new-relative-slider').owlCarousel({
+  loop: true,
+  margin: 30,
+  autoplay: true,
+  autoplayTimeout: 993000,
+  nav: true,
+  navText: ['<img srcset="../img/arrow-left-gray.png 2x" alt="" />', '<img srcset="../img/arrow-right.png 2x" alt="" />'],
+  responsive: {
+    0: {
+      items: 2
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 3
     }
   }
 })
