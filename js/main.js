6d060730ee3bdbaf28d12165fdf89294992b5658
Pham Tu <phamtu613@gmail.com>
2021-11-17 21:29:29 +0700

    add page room

--- js/main.js
+++ js/main.js
@@ -55,3 +55,22 @@ $('.new-relative-slider').owlCarousel({
     }
   }
 })
+$('.room-relative-slider').owlCarousel({
+  loop: true,
+  margin: 30,
+  autoplay: true,
+  autoplayTimeout: 993000,
+  nav: true,
+  navText: ['<img srcset="../img/arrow-left-gray.png 2x" alt="" />', '<img srcset="../img/arrow-right.png 2x" alt="" />'],
+  responsive: {
+    0: {
+      items: 2
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 3
+    }
+  }
+})
